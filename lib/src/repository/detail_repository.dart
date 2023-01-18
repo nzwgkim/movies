88e3f2cc7d2b32208c232c9ba45a075854cf4e51
Woody <nz.wgkim@gmail.com>
2023-01-18 13:40:07 +1300

    added cars.json at web

---
+++ lib/src/repository/detail_repository.dart
@@ -0,0 +1,11 @@
+class DetailRepository {
+  void detailMovie() async {
+    // https://api.themoviedb.org/3/movie/76600?api_key=be7a5086ca839d1d338f5d6789a92451
+    var query = {
+      'api_key': 'be7a5086ca839d1d338f5d6789a92451'
+    }; // https://www.themoviedb.org/ 제공하는 api key
+    var path = '/3/movie/popular';
+    var authority = 'api.themoviedb.org';
+    var uri = Uri.https(authority, path, query);
+  }
+}
